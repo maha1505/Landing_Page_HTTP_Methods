1e8045e78ccc1fff607c74e6a7be15b5b7791f2f
Mahalakshmi J <165121768+maha1505@users.noreply.github.com>
2025-02-12 16:20:27 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "MAHAKAN",  // Replace with your MySQL password
+  password: "",  // Replace with your MySQL password
   database: "academy",
 });
 
